bad4882f7680228dfb3a67aa5957f75f9273b9c4
KevinTamCS <kevin@kevintamcs.com>
2020-08-21 19:23:37 -0400

    Add MySQL connection

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const admin = require('firebase-admin');
-const mailchimp = require('@mailchimp/mailchimp_marketing');
-const sanitize = require('mongo-sanitize');
-const { v4: uuidv4 } = require('uuid');
-const md5 = require('md5');
+const admin = require("firebase-admin");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+const sequelize = require("sequelize");
+const sanitize = require("mongo-sanitize");
+const { v4: uuidv4 } = require("uuid");
+const md5 = require("md5");
+const { Sequelize } = require("sequelize");
 
+// Initialize Cloud Firestore
 admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.applicationDefault(),
 });
 const db = admin.firestore();
 
+// Initialize MySQL
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+});
+
+// Initialize Mailchimp
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_SERVER
+  server: process.env.MAILCHIMP_SERVER,
 });
 
 /**
@@ -24,28 +37,28 @@ mailchimp.setConfig({
  */
 exports.handleJoin = async (req, res) => {
   // Check for requests other than POST
-  if (req.method !== 'POST') {
-    console.error('Invalid request type!');
-    res.status(405).send('Method Not Allowed');
+  if (req.method !== "POST") {
+    console.error("Invalid request type!");
+    res.status(405).send("Method Not Allowed");
     return;
   }
 
   // Check for correct referer
   if (
-    !(req.get('referer').substring(0, 27) === 'https://bccompsci.club/join')
+    !(req.get("referer").substring(0, 27) === "https://bccompsci.club/join")
   ) {
-    console.error('Incorrect referer!');
-    console.log('Referer: ' + req.get('referer'));
-    res.status(403).send('Forbidden');
+    console.error("Incorrect referer!");
+    console.log("Referer: " + req.get("referer"));
+    res.status(403).send("Forbidden");
     return;
   }
 
   // Sanitize and parse inputs
   const body = req.body;
 
-  const firstName = sanitize(body['first-name']);
-  const lastName = sanitize(body['last-name']);
-  const email = sanitize(body['email']);
+  const firstName = sanitize(body["first-name"]);
+  const lastName = sanitize(body["last-name"]);
+  const email = sanitize(body["email"]);
 
   console.log(
     `${firstName} ${lastName} is requesting to join the club with email ${email}.`
@@ -54,35 +67,74 @@ exports.handleJoin = async (req, res) => {
   // Validate data before adding to database
   if (
     // Validate form data
-    firstName === '' ||
-    lastName === '' ||
-    !email.includes('@') ||
+    firstName === "" ||
+    lastName === "" ||
+    !email.includes("@") ||
     // Validate data types
-    typeof firstName !== 'string' ||
-    typeof lastName !== 'string' ||
-    typeof email !== 'string'
+    typeof firstName !== "string" ||
+    typeof lastName !== "string" ||
+    typeof email !== "string"
   ) {
-    console.error('Form data invalid!');
-    res.status(400).send('Bad Request');
+    console.error("Form data invalid!");
+    res.status(400).send("Bad Request");
     return;
   }
 
+  // Cloud Firestore database
   // Generate unique document ID
   const docId = `${firstName} ${lastName} ${email} ${uuidv4()}`;
-  const docRef = db.collection('members').doc(docId);
+  const docRef = db.collection("members").doc(docId);
 
-  // Add member to database
+  // Add the member
   await docRef.set({
     firstName: firstName,
     lastName: lastName,
     email: email,
-    joinDate: new Date()
+    joinDate: new Date(),
   });
+
+  // MySQL database
+  // Define the model
+  const Member = sequelize.define("Member", {
+    firstName: {
+      type: sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    joinDate: {
+      type: sequelize.DataTypes.TIME,
+      allowNull: false,
+    },
+  });
+
+  // Connect and add the member
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to MySQL Database");
+
+    await Member.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      joinDate: new Date(),
+    });
+    console.log("Added new member to MySQL Database.");
+  } catch (error) {
+    console.error(`Unable to connect to MySQL Database. Reason: ${error}`);
+  }
 
   console.log(
     `${firstName} ${lastName} has joined the club with email ${email}.`
   );
 
+  // Mailchimp API
   const listId = process.env.MAILCHIMP_MAILING_LIST_ID; // ID for the main mailing list.
   const subscriberHash = md5(email.toLowerCase());
 
@@ -104,11 +156,11 @@ exports.handleJoin = async (req, res) => {
 
         const response = await mailchimp.lists.addListMember(listId, {
           email_address: email,
-          status: 'subscribed',
+          status: "subscribed",
           merge_fields: {
             FNAME: firstName,
-            LNAME: lastName
-          }
+            LNAME: lastName,
+          },
         });
 
         console.log(
@@ -118,8 +170,9 @@ exports.handleJoin = async (req, res) => {
     }
   }
 
+  // Subscribe the member
   await processSubscribe();
 
   // Redirect to welcome page
-  res.redirect('https://bccompsci.club/welcome');
+  res.redirect("https://bccompsci.club/welcome");
 };
